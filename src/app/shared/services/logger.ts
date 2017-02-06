01a33ed35d4a0496c15e8c35fa56f4f49439c48c
Ajay Kumar <ajayak731@hotmail.com>
2017-02-06 21:06:19 +0530

    improved logger

--- src/app/shared/services/logger.ts
+++ src/app/shared/services/logger.ts
@@ -15,24 +15,36 @@ export class LogService {
   public log(message: string, data?: any): void {
     if (isProduction) { return; }
     console.log(message, data);
-    this.toastr.info({ titleText: message });
+    this.toastr.info({
+      title: message,
+      html: `<pre class="logger-text">${JSON.stringify(data, null, 2)}</pre>`
+    });
   }
 
   public error(message: string, data?: any) {
     if (isProduction) { return; }
     console.error(message, data);
-    this.toastr.error({ titleText: message });
+    this.toastr.error({
+      title: message,
+      html: `<pre class="logger-text">${JSON.stringify(data, null, 2)}</pre>`
+    });
   }
 
   public warn(message: string, data?: any) {
     if (isProduction) { return; }
     console.warn(message, data);
-    this.toastr.warn({ titleText: message });
+    this.toastr.warn({
+      title: message,
+      html: `<pre class="logger-text">${JSON.stringify(data, null, 2)}</pre>`
+    });
   }
 
   public info(message: string, data?: any) {
     if (isProduction) { return; }
     console.info(message, data);
-    this.toastr.info({ titleText: message });
+    this.toastr.info({
+      title: message,
+      html: `<pre class="logger-text">${JSON.stringify(data, null, 2)}</pre>`
+    });
   }
 }
