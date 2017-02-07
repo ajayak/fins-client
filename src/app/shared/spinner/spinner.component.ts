6790f3690f66a17e0ea723f6a96fb7790ac4b781
Ajay Kumar <ajayak731@hotmail.com>
2017-02-07 08:49:06 +0530

    added normalize.css

--- src/app/shared/spinner/spinner.component.ts
+++ src/app/shared/spinner/spinner.component.ts
@@ -1,7 +1,9 @@
 import {
   Component,
-  OnInit
+  OnInit,
+  OnDestroy
 } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 import { Store } from '../store';
 
@@ -9,18 +11,23 @@ import { Store } from '../store';
   selector: 'fs-spinner',
   template: `
   <div *ngIf="visible">
-    <md-spinner color="accent"></md-spinner>
+    <md-progress-bar mode="indeterminate"></md-progress-bar>
   </div>
   `
 })
-export class SpinnerComponent implements OnInit {
+export class SpinnerComponent implements OnInit, OnDestroy {
   public visible: boolean = false;
+  private subscription: Subscription;
 
   constructor(private store: Store) { }
 
-  public ngOnInit() {
-    this.store.changes
+  public ngOnInit(): void {
+    this.subscription = this.store.changes
       .map(state => state.showSpinner)
       .subscribe(spinnerState => this.visible = spinnerState);
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
