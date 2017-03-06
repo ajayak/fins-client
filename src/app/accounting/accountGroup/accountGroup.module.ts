0ca3402dc30c6aa9498d3586f0e8c488091495ae
Ajay Kumar <ajayak731@hotmail.com>
2017-03-06 10:06:20 +0530

    refactored account group routing code

--- src/app/accounting/accountGroup/accountGroup.module.ts
+++ src/app/accounting/accountGroup/accountGroup.module.ts
@@ -5,21 +5,26 @@ import {
   AccountGroupService,
   AccountGroupGuard
 } from './shared';
-import {
-  routedComponents,
-  entryComponents
-} from './accountGroup.route';
+import { routedComponents } from './accountGroup.route';
+import { AccountGroupTreeComponent } from './accountGroup-tree';
+import { AddAccountGroupComponent } from './accountGroup-adder';
+import { AccountGroupCreatorDialogComponent } from './accountGroup-creator';
 
 @NgModule({
   imports: [
     SharedModule,
   ],
   exports: [],
-  declarations: [routedComponents],
+  declarations: [
+    routedComponents,
+    AccountGroupTreeComponent,
+    AddAccountGroupComponent,
+    AccountGroupCreatorDialogComponent
+  ],
   providers: [
     AccountGroupService,
     AccountGroupGuard
   ],
-  entryComponents: [entryComponents]
+  entryComponents: [AccountGroupCreatorDialogComponent]
 })
 export class AccountGroupModule { }
--- src/app/accounting/accountGroup/accountGroup.route.ts
+++ src/app/accounting/accountGroup/accountGroup.route.ts
@@ -1,22 +1,16 @@
 import { Routes } from '@angular/router';
 
 import { AccountGroupContainer } from './accountGroup.container';
-import { AccountGroupTreeComponent } from './accountGroup-tree';
-import { AddAccountGroupComponent } from './accountGroup-adder';
-import { AccountGroupCreatorDialogComponent } from './accountGroup-creator';
 import { AccountGroupGuard } from './shared';
 
 export const accountGroupRoutes: Routes = [
-  { path: 'account-group', component: AccountGroupContainer, canActivate: [AccountGroupGuard] }
+  {
+    path: 'account-group',
+    component: AccountGroupContainer,
+    canActivate: [AccountGroupGuard]
+  }
 ];
 
 export const routedComponents = [
   AccountGroupContainer,
-  AccountGroupTreeComponent,
-  AddAccountGroupComponent,
-  AccountGroupCreatorDialogComponent
-];
-
-export const entryComponents = [
-  AccountGroupCreatorDialogComponent
 ];
