09baac25c0f7f95aac03e3285017bc28ebff2a7c
Ajay Kumar <ajayak731@hotmail.com>
2017-04-01 15:27:10 +0530

    refactored styles

--- src/app/home/home.container.ts
+++ src/app/home/home.container.ts
@@ -13,19 +13,16 @@ import {
 @Component({
   selector: 'fs-home',
   template: `
-    <div [class]="selectedTheme">
-      <fs-navbar></fs-navbar>
-      <fs-sidenav [open]="isSideNavOpen">
-        <router-outlet></router-outlet>
-      </fs-sidenav>
-    <div>
+    <fs-navbar></fs-navbar>
+    <fs-sidenav [open]="isSideNavOpen">
+      <router-outlet></router-outlet>
+    </fs-sidenav>
   `
 })
 // tslint:disable-next-line:component-class-suffix
 export class HomeContainer implements OnInit, OnDestroy {
   public subscription: Subscription;
   public isSideNavOpen = false;
-  public selectedTheme: string;
 
   constructor(private store: Store) { }
 
@@ -33,11 +30,15 @@ export class HomeContainer implements OnInit, OnDestroy {
     this.subscription = this.store.changes
       .subscribe((state: State) => {
         this.isSideNavOpen = state.openSideNav;
-        this.selectedTheme = state.selectedTheme;
+        this.setBodyTheme(state.selectedTheme);
       });
   }
 
   public ngOnDestroy() {
     this.subscription.unsubscribe();
+  }
+
+  private setBodyTheme(theme: string) {
+    document.querySelector('body').setAttribute('class', theme);
   }
 }
--- src/app/home/navbar/navbar.container.ts
+++ src/app/home/navbar/navbar.container.ts
@@ -8,17 +8,18 @@ import { Subscription } from 'rxjs/Subscription';
 import { Router } from '@angular/router';
 
 import { SideNavService } from '../sidenav';
-import {
-  Store,
-  State
-} from '../../shared';
+import { Store } from '../../shared';
 import { AuthService } from '../../auth/shared';
 
 @Component({
   selector: 'fs-navbar',
   templateUrl: 'navbar.container.html',
-  styleUrls: ['./navbar.container.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: [`
+  .spacer {
+    flex: 1 1 auto;
+  }
+  `]
 })
 // tslint:disable-next-line:component-class-suffix
 export class NavbarContainer implements OnInit, OnDestroy {
@@ -38,18 +39,12 @@ export class NavbarContainer implements OnInit, OnDestroy {
 
   public ngOnInit() {
     this.sideNavSubscription = this.store.changes
-      .subscribe((state: State) => {
-        this.openSideNav = state.openSideNav;
-        this.selectTheme(state.selectedTheme);
-      });
+      .map(state => state.openSideNav)
+      .subscribe(sideNavState => this.openSideNav = sideNavState);
   }
 
   public ngOnDestroy() {
     this.sideNavSubscription.unsubscribe();
-  }
-
-  public selectTheme(theme: string) {
-    console.log(theme);
   }
 
   public signout() {
